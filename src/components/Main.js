742de031ed7fa6e5b4cae41349e44806fdcf7c37
pigflymoon <pplflymoon2015@gmail.com>
2017-01-10 20:25:29 +1300

    #install node-sass and sass-loader

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,35 +4,22 @@ require('styles/App.scss');
 import React from 'react';
 
 //获取图片相关的数据
-let imageDatas = require('../data/imageDatas.json');
-
-//利用自执行函数,将图片名信息转化为图片url路径信息
-imageDatas = (function genImageURL(imageDatasArr) {
-    for (var i = 0, j = imageDatasArr.length; i < j; i++) {
-        var singleImageData = imageDatasArr[i];
-
-        singleImageData.imageURL = require('../images' +
-            singleImageData.fileName);
-        imageDatasArr[i] = singleImageData;
-    }
-
-    return imageDatasArr;
-})(imageDatas);
 
 
-class GalleryByReactApp extends React.Component {
+let yeomanImage = require('../images/1.jpg');
+
+class AppComponent extends React.Component {
     render() {
         return (
-            <section className="stage">
-                <section className="img-sec">
-                </section>
-                <nav className="controller-nav">
-                </nav>
-            </section>
+            <div className="index">
+                <img src={yeomanImage} alt="Yeoman Generator" />
+                <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
+            </div>
         );
     }
 }
 
-GalleryByReactApp.defaultProps = {};
+AppComponent.defaultProps = {
+};
 
-export default GalleryByReactApp;
+export default AppComponent;
